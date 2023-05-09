0dd0403a329c97ba26c6832074dcd471365c1c9b
Patricio Lagos <115107187+patricio97@users.noreply.github.com>
2023-05-08 22:22:07 -0400

    1.0

--- metajuegos-1-main (1)/metajuegos-1-main/MetaJuegos_Django/insert.sql
+++ metajuegos-1-main (1)/metajuegos-1-main/MetaJuegos_Django/insert.sql
@@ -4,4 +4,4 @@ INSERT INTO core_juego VALUES(3, 0000111, 'Mario Straiker', 002, 003);
 INSERT INTO core_juego VALUES(4, 0001111, 'Diablo III', 003, 001);
 INSERT INTO core_juego VALUES(5, 0011111, 'Pokemon', 004, 003);
 INSERT INTO core_juego VALUES(6, 0111111, 'Resident Evil', 005, 001);
-INSERT INTO core_juego VALUES(6, 0111111, 'CS GO', 001, 004);
+INSERT INTO core_juego VALUES(7, 1111111, 'CS GO', 001, 004);
